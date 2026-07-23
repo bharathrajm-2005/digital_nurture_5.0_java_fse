dc4ca0ea31ea0981c2b736430d1827c1bbc49e87
Bharath Raj <mbharathrajcw@gmail.com>
2026-07-23 12:40:19 +0530

    The week 6 GIT Handson

--- WEEK - 6/12_ReactJS_HOL/ticketbookingapp/src/Components/GuestGreeting.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function GuestGreeting() {
-  return (
-    <h2>Please sign up.</h2>
-  );
-}
-
-export default GuestGreeting;
--- WEEK - 6/12_ReactJS_HOL/ticketbookingapp/src/Components/UserGreeting.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function UserGreeting() {
-  return (
-    <h2>Welcome back</h2>
-  );
-}
-
-export default UserGreeting;
